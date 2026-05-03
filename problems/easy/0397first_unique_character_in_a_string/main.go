c53899c9ce662d56596550331b3a70c55050a76c
hvnrstrd <heavenrestricted@gmail.com>
2026-05-03 19:15:55 +0500

    leetcode go solution: 0397first_unique_character_in_a_string

--- problems/easy/0397first_unique_character_in_a_string/main.go
+++ problems/easy/0397first_unique_character_in_a_string/main.go
@@ -1,10 +1,29 @@
+package main
+
+import "fmt"
+
+// First Unique Character in a String
+// Complexity - Time: O(n) - Space: O(1)
+// Pattern: Hash Map
+// Count character frequencies, then find first with count == 1.
+
 func firstUniqChar(s string) int {
-mapChar := make(map[byte]int)
-for i := 0; i < len(s); i++ {
-    mapChar[s[i]]++
+	mapChar := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		mapChar[s[i]]++
+	}
+	for i := 0; i < len(s); i++ {
+		if mapChar[s[i]] == 1 {
+			return i
+		}
+	}
+	return -1
 }
-for i := 0; i < len(s); i++ {
-    if mapChar[s[i]] == 1 {
-        return i
-    }
+
+// ------------------------------------------------------------------
+
+func main() {
+	fmt.Println(firstUniqChar("leetcode"))
+	fmt.Println(firstUniqChar("loveleetcode"))
+	fmt.Println(firstUniqChar("aabb"))
 }
